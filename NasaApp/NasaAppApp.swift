89b4d0090bfb720672a7093934c6c5827674dc7c
tylertorres <tylertorresp@gmail.com>
2023-09-23 13:46:29 -0400

    Initial Commit

---
+++ NasaApp/NasaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAppApp.swift
+//  NasaApp
+//
+//  Created by Tyler Torres on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NasaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
