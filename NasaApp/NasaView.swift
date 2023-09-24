e5302ea8540c91e4c7318f7c3039cd32bfecdd2f
tylertorres <tylertorresp@gmail.com>
2023-09-24 16:21:50 -0400

    minor tweak in UI ; images frame is not needed since the parents' is propagated to it

--- NasaApp/NasaView.swift
+++ NasaApp/NasaView.swift
@@ -20,17 +20,16 @@ struct NasaView: View {
                     .resizable()
                     .scaledToFit()
                     .clipShape(RoundedRectangle(cornerRadius: 25))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
 
                 ScrollView(showsIndicators: false) {
                     Text(viewModel.apod?.explanation ?? "")
                         .font(.body)
                         .lineSpacing(5)
-//                        .padding()
+                        .padding()
                 }
             }
-            .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
             .onAppear { viewModel.fetchAPOD() }
             .navigationTitle(viewModel.apod?.title ?? "Title")
             .navigationBarTitleDisplayMode(.large)
